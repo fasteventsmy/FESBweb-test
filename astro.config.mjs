37bfcb8bd974a489154d763ca7e0fef33854095f
fasteventsmy <creatives@fasteventsmalaysia.com>
2025-10-12 03:37:27 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-
-// Your repo name here:
-const base = '/FESbweb-test/';
-
 export default defineConfig({
   output: 'static',
-  base,
+  // base: '/FESbweb-test/'   // <- remove this line for Vercel
 });
