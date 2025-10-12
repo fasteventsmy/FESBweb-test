6bc448b4903c47918ebe7c9833b0b13e4863159c
fasteventsmy <creatives@fasteventsmalaysia.com>
2025-10-13 02:08:11 +0800

    Delete src directory

--- src/utils/getServices.ts
+++
@@ -1,1 +0,0 @@
-import services from '/content/services.json'; export default function getServices(){ return services as any[]; }
--- src/utils/getTeam.ts
+++
@@ -1,1 +0,0 @@
-import team from '/content/team/people.json'; export default function getTeam(){ return team as any[]; }
